67ca31d664fdef4a32fe20675e497dfa1c26253d
Jake Brukhman <jake@coinfund.io>
2023-05-06 21:16:55 -0400

    Very basic working Jargon terminal.

--- jargon_cli/cli.py
+++ jargon_cli/cli.py
@@ -1,4 +1,21 @@
 import os
+import pkg_resources
+
+from langchain.prompts import (
+    ChatPromptTemplate, 
+    MessagesPlaceholder, 
+    SystemMessagePromptTemplate, 
+    HumanMessagePromptTemplate,
+    PromptTemplate
+)
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+from langchain.chains import ConversationChain
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
 
 class JargonCli:
 
@@ -17,3 +34,35 @@ class JargonCli:
         for file in os.listdir(self.jargdir):
             if file.endswith(".jarg"):
                 yield file
+  
+    def execute(self, proc):
+        if not proc.endswith('.jarg'):
+            proc += '.jarg'
+        procpath = os.path.join(self.jargdir, proc)
+        if not os.path.exists(procpath):
+            raise Exception(f'Unknown procedure: {procpath}')
+        
+        # read the Jargon spec
+        jargon_prompt = pkg_resources.resource_string(__name__, '../jargon.md').decode('utf-8')
+        print(jargon_prompt)
+        prompt = ChatPromptTemplate.from_messages([
+            SystemMessagePromptTemplate(prompt=PromptTemplate(template=jargon_prompt.replace('{', '{{').replace('}', '}}'), input_variables=[])),
+            MessagesPlaceholder(variable_name="history"),
+            HumanMessagePromptTemplate.from_template("{input}")
+        ])
+
+        llm = ChatOpenAI(temperature=0, model_name='gpt-4')
+        memory = ConversationBufferMemory(return_messages=True)
+        conversation = ConversationChain(memory=memory, prompt=prompt, llm=llm)
+
+        with open(procpath, 'r') as file:
+            proctxt = file.read()
+            print(proctxt)
+            result = conversation.predict(input=f"Execute the following Jargon procedure and print only its output:\n{proctxt}")
+            print(result)
+
+        while True:
+            # print(memory.chat_memory)
+            txt = input('\n> ')
+            result = conversation.predict(input=txt)
+            print(result)
--- jargon_cli/main.py
+++ jargon_cli/main.py
@@ -3,29 +3,44 @@ import os
 
 from .cli import JargonCli
 
-@click.group()
+@click.group(invoke_without_command=True)
 @click.option('--jargon-dir', '-d', envvar='JARGON_DIR', default=os.path.expanduser('~/.jargon'), help="Manually set the Jargon directory.", type=str)
 @click.pass_context
 def cli(ctx, jargon_dir):
     ctx.obj = JargonCli(jargon_dir=jargon_dir)
+    if ctx.invoked_subcommand is None:
+        click.echo(ctx.get_help())
 
 @click.command()
 @click.pass_context
 def ls(ctx):
     '''List available Jargon procedures.'''
     for proc in ctx.obj.ls():
-        print('--', proc)
+        click.echo(f'* {proc}')
 
 @click.command()
-def execute():
+@click.argument('proc')
+@click.pass_context
+def execute(ctx, proc):
     '''Execute a known procedure.'''
-    pass
+    try:
+        ctx.obj.execute(proc)
+    except Exception as e:
+        click.echo(f"ERROR: {e}")
 
 @click.command()
-# @click.option('--name', default='User', help='Your name')
-def edit():
+@click.argument('jargfile')
+@click.pass_context
+def edit(ctx, jargfile):
     '''Edit a procedure.'''
-    pass
+    jargdir = ctx.obj.jargdir
+    if not jargfile.endswith('.jarg'):
+        jargfile += '.jarg'
+    filepath = os.path.join(jargdir, jargfile)
+    if not os.path.exists(filepath):
+        with open(filepath, 'w') as file:
+            file.write(f'+++ {jargfile[:-5]}\n\n+++')
+    click.edit(filename=filepath)
 
 cli.add_command(ls)
 cli.add_command(execute)
